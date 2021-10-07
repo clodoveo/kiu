e659950420b2a3c8102a828af5f5ce0e6a608106
clodoveo <filippo@ramointeractive.com>
2021-10-07 19:12:05 +0200

    vanti

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { useQuery, useQueryClient } from "react-query";
 
 export default function Splash() {
-  return <div>Splash</div>;
+  // Queries
+  const query = useQuery("todos", getTodos);
+
+  return (
+    <div
+      style={{
+        maxWdth: "700px",
+        margin: "0 auto",
+        backgroundColor: "#999"
+      }}
+    >
+      {query.data && query.data.map((i) => <ul key={i.id}>{i.title}</ul>)}
+    </div>
+  );
+}
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  const data = await res.json();
+  console.log(data);
+  return data;
 }
